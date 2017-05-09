f50a7ddc0367184e70c2453ebe4d4976868c4e0b
VerDan777 <vereshhagin98@mail.ru>
2017-05-09 12:51:53 +0200

    Hello world

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-console.log("works");
+import React from "react";
+import {render} from "react-dom";
+
+ class App extends React.Component{
+     render() {
+         return(
+         <div>
+             <h1>Hello!!1</h1>
+         </div>
+         )
+     }
+ }
+render(<App/>,document.getElementById("app"));
