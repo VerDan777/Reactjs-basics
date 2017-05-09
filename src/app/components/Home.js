a5a8eb5cb563a008c25381a7e4f7563cd68933e7
VerDan777 <vereshhagin98@mail.ru>
2017-05-09 16:03:40 +0200

    new components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,10 +2,18 @@ import React from "react";
 
 export class Home extends React.Component {
     render() {
+        var text= "text";
         return (
             <div>
                 <p>In a new Component!</p>
+                <p>{text}</p>
+                <p>My name {this.props.name},my age is {this.props.age}</p>
             </div>
+           
         );
     }
+  
 }
+  Home.propTypes= {
+        name:React.propTypes.string,
+    };
--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,10 @@ import {Header} from './components/Header';
 import { Home } from "./components/Home";
  class App extends React.Component{
      render() {
+        var user={
+             name:"Daniil",
+             hobbies:["sport","programming"]
+         };
          return(
          <div className="container">
              <div className="row">
@@ -14,6 +18,7 @@ import { Home } from "./components/Home";
               <div className="row">
                  <div className="col-xs-10 col-xs-offset-1">
              <Home/>
+             <Home name={"Daniil"} age={18} user={user}/>
                 </div>
              </div>
          </div>
