ca9e9df238c04b7f2f17c2210ce6ca20a1e12380
VerDan777 <vereshhagin98@mail.ru>
2017-05-10 09:55:35 +0200

    added btn

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age=props.age;
+    }
+    OnMakeChanged() {
+        this.age+=3;
+        console.log(this.age);
+    }
     render() {
         var text= "text";
         return (
             <div>
                 <p>In a new Component!</p>
                 <p>{text}</p>
-                <p>My name {this.props.name},my age is {this.props.age}</p>
+                {/*<p>My name {this.props.name},my age is {this.props.age}</p>*/}
+                <p>My name {this.props.name},my age is {this.age}</p>
+                <button className="btn btn-primary" onClick={() => this.OnMakeChanged()}>Click</button>
             </div>
            
         );
